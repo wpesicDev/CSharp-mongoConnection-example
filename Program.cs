516f38487c52a5ee3e94d8b64e5c6e4d7a6a3e33
wpesicDev <williams.pesic@gmail.com>
2024-05-16 11:51:06 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient dbClient = new MongoClient("mongodb://gbs:secret@localhost:27017");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
